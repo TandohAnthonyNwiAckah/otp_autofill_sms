a927e917c8a5438c5d331b0b28b5a635f6a5e8b2
Tandoh Anthony Nwi-Ackah <anthony.tandoh@tanacom.io>
2024-09-14 21:16:44 +0000

    added : SMS autofill example

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sms_otp_autofill/sms_otp_auto_fill.dart';
@@ -6,19 +9,28 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class MyAppState extends State<MyApp> {
+  // String _platformVersion = 'Unknown';
+
+  final int _otpCodeLength = 4;
+  bool _isLoadingButton = false;
+  bool _enableButton = false;
+  String _otpCode = "";
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final intRegex = RegExp(r'\d+', multiLine: true);
+  TextEditingController textEditingController = TextEditingController(text: "");
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    _getSignatureCode();
+    _startListeningSms();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
@@ -36,7 +48,82 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      if (kDebugMode) {
+        print(platformVersion);
+      }
+      // _platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SmsAutofill.stopListening();
+  }
+
+  BoxDecoration get _pinPutDecoration {
+    return BoxDecoration(
+      border: Border.all(color: Theme.of(context).primaryColor),
+      borderRadius: BorderRadius.circular(15.0),
+    );
+  }
+
+  /// get signature code
+  _getSignatureCode() async {
+    String? signature = await SmsAutofill.getAppSignature();
+    if (kDebugMode) {
+      print("signature $signature");
+    }
+  }
+
+  /// listen sms
+  _startListeningSms() {
+    SmsAutofill.startListeningSms().then((message) {
+      setState(() {
+        _otpCode = SmsAutofill.getCode(message, intRegex);
+        textEditingController.text = _otpCode;
+        _onOtpCallBack(_otpCode, true);
+      });
+    });
+  }
+
+  _onSubmitOtp() {
+    setState(() {
+      _isLoadingButton = !_isLoadingButton;
+      _verifyOtpCode();
+    });
+  }
+
+  _onClickRetry() {
+    _startListeningSms();
+  }
+
+  _onOtpCallBack(String otpCode, bool isAutofill) {
+    setState(() {
+      _otpCode = otpCode;
+      if (otpCode.length == _otpCodeLength && isAutofill) {
+        _enableButton = false;
+        _isLoadingButton = true;
+        _verifyOtpCode();
+      } else if (otpCode.length == _otpCodeLength && !isAutofill) {
+        _enableButton = true;
+        _isLoadingButton = false;
+      } else {
+        _enableButton = false;
+      }
+    });
+  }
+
+  _verifyOtpCode() {
+    FocusScope.of(context).requestFocus(FocusNode());
+    Timer(const Duration(milliseconds: 4000), () {
+      setState(() {
+        _isLoadingButton = false;
+        _enableButton = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Verification OTP Code $_otpCode Success")));
     });
   }
 
@@ -44,13 +131,81 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OtpTextField(
+                    textController: textEditingController,
+                    autoFocus: true,
+                    codeLength: _otpCodeLength,
+                    alignment: MainAxisAlignment.center,
+                    defaultBoxSize: 46.0,
+                    margin: 10,
+                    selectedBoxSize: 46.0,
+                    textStyle: const TextStyle(fontSize: 16),
+                    defaultDecoration: _pinPutDecoration.copyWith(
+                        border: Border.all(
+                            color: Theme.of(context)
+                                .primaryColor
+                                .withOpacity(0.6))),
+                    selectedDecoration: _pinPutDecoration,
+                    onChange: (code) {
+                      _onOtpCallBack(code, false);
+                    }),
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: MaterialButton(
+                    onPressed: _enableButton ? _onSubmitOtp : null,
+                    color: Colors.blue,
+                    disabledColor: Colors.blue[100],
+                    child: _setUpButtonChild(),
+                  ),
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: TextButton(
+                    onPressed: _onClickRetry,
+                    child: const Text(
+                      "Retry",
+                      style: TextStyle(color: Colors.orange),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  Widget _setUpButtonChild() {
+    if (_isLoadingButton) {
+      return const SizedBox(
+        width: 19,
+        height: 19,
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        ),
+      );
+    }
+    //
+    else {
+      return const Text(
+        "Verify",
+        style: TextStyle(color: Colors.white),
+      );
+    }
   }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:sms_otp_autofill_example/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
